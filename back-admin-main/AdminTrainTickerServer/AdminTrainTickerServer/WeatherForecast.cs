10f419d246490119752fd039c757930606baa77c
dihepp <29692155+dihepp@users.noreply.github.com>
2024-09-18 19:49:01 +0300

    Update WeatherForecast.cs

--- back-admin-main/AdminTrainTickerServer/AdminTrainTickerServer/WeatherForecast.cs
+++ back-admin-main/AdminTrainTickerServer/AdminTrainTickerServer/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace AdminTrainTickerServer
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 3200 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
